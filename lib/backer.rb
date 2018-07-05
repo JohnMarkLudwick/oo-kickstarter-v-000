da91514f327443666c7fed936c363ef3dfbcc0c8
JohnMark <johnmarkludwick@gmail.com>
2018-07-05 16:11:02 +0000

    automatically backed up by learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -10,10 +10,18 @@ class Backer
   
   def back_project(project)
     @backed_projects << project
+<<<<<<< HEAD
     project.backers << self
 
   end
 
+=======
+    @backer
+  end
+  
+  
+  
+>>>>>>> ad0c25476e8a909f0898c0514d50eb23c6ca82f8
   
 end
 
--- lib/project.rb
+++ lib/project.rb
@@ -11,9 +11,22 @@ class Project
   
   def add_backer(backer)
     @backers << backer
+<<<<<<< HEAD
     backer.backed_projects << self
   end
   
 end
 
 
+=======
+  end
+  
+  
+  
+end
+
+
+
+
+
+>>>>>>> ad0c25476e8a909f0898c0514d50eb23c6ca82f8
